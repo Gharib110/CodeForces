73756113111e53a49a763ff56e1193be88b4755e
AlirezaGharib <alirezagharib110@gmail.com>
2021-10-17 11:02:10 +0330

    IntegerToRoman

---
+++ IntegerToRoman/IntegerToRoman.cpp
@@ -0,0 +1,38 @@
+// IntegerToRoman.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    unordered_map<int, string> cons;
+    cons[1] = "I";
+    cons[5] = "V";
+    cons[10] = "X";
+    cons[50] = "L";
+    cons[100] = "C";
+    cons[500] = "D";
+    cons[1000] = "M";
+
+    for (unordered_map<int, string>::iterator iterator = cons.begin() ; iterator != cons.end(); iterator++)
+    {
+        
+    }
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
