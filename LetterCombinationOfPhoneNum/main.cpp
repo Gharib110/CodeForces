da4a2281dc7887aecc042ccd75230a95876f67d7
AlirezaGharib <alirezagharib110@gmail.com>
2021-10-21 10:50:16 +0330

    solved

--- LetterCombinationOfPhoneNum/main.cpp
+++ LetterCombinationOfPhoneNum/main.cpp
@@ -4,12 +4,32 @@
 
 using namespace std;
 
-vector<string> letterCombinations(string digits) {
+int letterCombinations(string digits) {
     auto* data = new unordered_map<char, string>{};
-    data->insert(make_pair('2', "")); // This is so important
+    data->insert(make_pair('2', "abc")); // This is so important to remember
+    data->insert(make_pair('3', "def"));
+    data->insert(make_pair('4', "ghi"));
+    data->insert(make_pair('5', "jkl"));
+    data->insert(make_pair('6', "mno"));
+    data->insert(make_pair('7', "pqrs"));
+    data->insert(make_pair('8', "tuv"));
+    data->insert(make_pair('9', "wxyz"));
+
+    vector<string> output;
+
+    string digOne = data->at(digits[0]);
+
+    for (int i = 0; i < digOne.length(); ++i) {
+
+    }
+
+    delete data;
+
+    return NULL;
 }
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    letterCombinations("34314");
     return 0;
 }
